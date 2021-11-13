0139c801a46ffb6d655c0bbeea5200792d1b3fe0
RUIN FF <ibrahimaulia969@gmail.com>
2021-11-13 16:32:03 +0700

    rest-apikey-xbot

---
+++ index.js
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="og:image" content="https://g.top4top.io/p_2141ke1wv0.jpg" />
+    <title>PROFIL RUIN FF</title>
+    <link rel="stylesheet" href="/st2">
+    	<form action="/api">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="img-area">
+      <div class="inner-area">
+        <img src="https://a.top4top.io/p_21422h6t20.jpg" alt="">
+      </div>
+    </div>
+    <a href="/apis" class="icon arrow"><i class="fas fa-arrow-left"></i></a>
+    <div class="name">Lazuardi</div>
+    <div class="about">Free Account</div>
+    
+    <div class="social-icons">
+      <a href="#" class="fb"><i class="fab fa-facebook-f"></i></a>
+      <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com/ardiwj13/" class="insta"><i class="fab fa-instagram"></i></a>
+      <a href="https://m.youtube.com/channel/UCqkpkST3ElnoHNAwEcuRX3g" class="yt"><i class="fab fa-youtube"></i></a>
+    </div>
+    <div class="buttons">
+      <button onclick="window.location=
+'/api' "style="height: 50px; width: 350px;">
+<b>
+Logout</b>
+    </div>
+    <div class="about">•</div>
+    <div class="about">Username : XbotUser_</div>
+    <div class="about">UserId : 1627737673</div>
+    <div class="about">Status : Public</div>
+    <div class="about">User : Online</div>
+    <div class="about">Status : Public</div>
+    <div class="social-share">
+      <div class="row"><img src="https://k.top4top.io/p_2142rcs7t0.png" width="15" height="15">
+        <span>60 DAY</span>
+      </div>
+      <div class="row"><img src="https://d.top4top.io/p_214298c670.png" width="15" height="15">
+        <span>89 LV</span>
+      </div>
+      <div class="row"><img src="https://b.top4top.io/p_2142c9jwz0.png" width="15" height="15">
+        <span>709 Use</span>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
